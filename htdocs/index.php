086745b3ff09f64c881289c32faaf1a0ed70603a
kamil <kamil@P5K-PRO>
2017-02-17 19:34:03 +0100

    [touch:2871] naprawa konfiguracji serwera, logowanie i rejestracja

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,4 @@
 <?php
 
 include 'templates/index.html';
+
---
+++ htdocs/php/register_display.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_SESSION['user'])){
+    if (isset($_SERVER['HTTP_REFERER']) && strstr($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) !== false){
+        $redirect_url = $_SERVER['HTTP_REFERER'];
+    } else {
+        $redirect_url = $_SERVER['SERVER_NAME'];
+    }
+    
+    header('Location: ' . $redirect_url);
+    exit(0);
+}
+
+include '../templates/register.html';
